590d46330b10c86d6639f5f1aa21386860c8cb55
noxiss <kamiljakubczak@outlook.com>
2020-03-21 00:58:23 +0100

    Added inital models

--- recipes/api/admin.py
+++ recipes/api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Product, Ingridient, Meal, Category
 
 # Register your models here.
+admin.site.register(Product)
+admin.site.register(Ingridient)
+admin.site.register(Meal)
+admin.site.register(Category)
+
+
--- recipes/api/models.py
+++ recipes/api/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    proteins = models.DecimalField(max_digits=7, decimal_places=2)
+    carbohudrate = models.DecimalField(max_digits=7, decimal_places=2)
+    fat = models.DecimalField(max_digits=7, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.category_name
+
+class Ingridient(models.Model):
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+    grams = models.IntegerField(blank=True, null=True)
+    pieces = models.IntegerField(blank=True, null=True)
+    milimiters = models.IntegerField(blank=True, null=True)
+    description = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.description
+
+class Meal(models.Model):
+    name = models.CharField(max_length=255)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    ingridients = models.ManyToManyField(Ingridient)
+    
+    def __str__(self):
+        return self.name
